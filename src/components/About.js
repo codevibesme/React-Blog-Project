8c5f103ff9d6d859746acb036efa2ec46ad72bef
Soham <sohammalakarofficial@gmail.com>
2023-07-12 14:57:22 +0530

    Create a Blog App, Use React, React-router, in-built Hooks, and more updates coming!

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <main className="About">
+            <h2>About</h2>
+            <p style={{marginTop: "1rem"}}>
+                This blog app is created with React.JS, React Router and Hooks.
+            </p>
+        </main>
+    );
+}
+export default About;
